b01c39f6ebb5916cec5eec58a49de7eab5c359a5
samet <s.burgazoglu@gmail.com>
2022-09-05 19:17:46 +0300

    print values for debugging

--- web_backend_api/settings.py
+++ web_backend_api/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("STATIC_ROOT", STATIC_ROOT)
+print("CURRENT_PATH", os.getcwd())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
