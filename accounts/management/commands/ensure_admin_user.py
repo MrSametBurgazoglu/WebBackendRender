32b72da9c9be31dcbe912d2bbc81e1d70363bc72
samet <s.burgazoglu@gmail.com>
2022-09-07 17:33:03 +0300

    ensure_admin_user.py is created

---
+++ accounts/management/commands/ensure_admin_user.py
@@ -0,0 +1,14 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Creates an admin user non-interactively if it doesn't exist"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(username='admin',
+                                          email=os.environ.get('SECRET_EMAIL'),
+                                          password=os.environ.get('SECRET_PASSWORD'))
