b0ffd2351bdfef147a1a720055239ca627930347
Mr.Samet <s.burgazoglu@gmail.com>
2023-01-06 21:46:01 +0300

    bugfix
    Bugfix for ensure_admin_user script. Its bugged because it try to access database before migration.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py ensure_admin_user
 python manage.py migrate
+python manage.py ensure_admin_user
